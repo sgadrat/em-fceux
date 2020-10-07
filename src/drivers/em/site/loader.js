616dee5f7d5e9c87120843f51c39a65bdb052000
Sylvain Gadrat <sgadrat@wontfix.it>
2020-10-07 02:18:27 +0200

    DONE(realtime) Inform the player when her system perfs are not good

--- src/drivers/em/site/loader.js
+++ src/drivers/em/site/loader.js
@@ -21,6 +21,8 @@
 // NOTE: Originally from: http://jsfiddle.net/vWx8V/
 var KEY_CODE_TO_NAME = {8:"Backspace",9:"Tab",13:"Return",16:"Shift",17:"Ctrl",18:"Alt",19:"Pause/Break",20:"Caps Lock",27:"Esc",32:"Space",33:"Page Up",34:"Page Down",35:"End",36:"Home",37:"Left",38:"Up",39:"Right",40:"Down",45:"Insert",46:"Delete",48:"0",49:"1",50:"2",51:"3",52:"4",53:"5",54:"6",55:"7",56:"8",57:"9",65:"A",66:"B",67:"C",68:"D",69:"E",70:"F",71:"G",72:"H",73:"I",74:"J",75:"K",76:"L",77:"M",78:"N",79:"O",80:"P",81:"Q",82:"R",83:"S",84:"T",85:"U",86:"V",87:"W",88:"X",89:"Y",90:"Z",91:"Meta",93:"Right Click",96:"Numpad 0",97:"Numpad 1",98:"Numpad 2",99:"Numpad 3",100:"Numpad 4",101:"Numpad 5",102:"Numpad 6",103:"Numpad 7",104:"Numpad 8",105:"Numpad 9",106:"Numpad *",107:"Numpad +",109:"Numpad -",110:"Numpad .",111:"Numpad /",112:"F1",113:"F2",114:"F3",115:"F4",116:"F5",117:"F6",118:"F7",119:"F8",120:"F9",121:"F10",122:"F11",123:"F12",144:"Num Lock",145:"Scroll Lock",182:"My Computer",183:"My Calculator",186:";",187:"=",188:",",189:"-",190:".",191:"/",192:"`",219:"[",220:"\\",221:"]",222:"'"};
 
+var hack_first_input_time = null;
+
 var FCEM = {
 soundEnabled : true,
 showControls : (function(show) {
@@ -303,6 +305,15 @@ toggleSound : (function() {
 				send_clock += 1000;
 			}
 
+			//HACK Ensure at least 20 seconds passed since first input (to have a clean perf report, with audio enabled all the time)
+			if (hack_first_input_time !== null) {
+				hack_delay = hack_first_input_time + 20000 - Date.now();
+				if (hack_delay > send_clock) {
+					console.log("delaying ping result by "+ (hack_delay-send_clock) +" ms to check browser perf");
+					send_clock = hack_delay;
+				}
+			}
+
 			// One second after the last ping, finalize the operation
 			setTimeout(function() {
 				chan.close();
@@ -312,10 +323,28 @@ toggleSound : (function() {
 	},
 
 	popPingResult : function() {
+		// Return nothing if there is nothing to return
 		if (FCEM.pingRunning || FCEM.pingResult === null) {
 			return null;
 		}
 
+		// Warn or block online if emulator performances are too bad
+		let perf_estimate = FCEM.perfEstimation();
+		if (perf_estimate['summary'] === 'bad') {
+			console.warn('Performance warning: '+ JSON.stringify(perf_estimate));
+			GuiMessage.warn('Performance', 'Your browser seems to have difficulties running the game at real time.<br />If online is laggy, try the native emulator.');
+		}else if (perf_estimate['summary'] === 'very-bad') {
+			console.warn('Performance error: '+ JSON.stringify(perf_estimate));
+			GuiMessage.error('Performance', 'Your browser fails to emulate the game at real time.<br />You cannot play online with it, it would be too laggy.');
+
+			// Soft block access to online by never returning ping result
+			FCEM.pingResult = null;
+			return null;
+		}else {
+			console.log('Performance: '+ JSON.stringify(perf_estimate));
+		}
+
+		// Return ping result's summary
 		let result = {
 			min: 999999,
 			max: 0,
@@ -351,6 +380,27 @@ toggleSound : (function() {
 			result[info_names[info_idx]] = getValue(cpp_timing_table_addr + 4*info_idx, 'i32');
 		}
 		return result;
+	},
+	perfEstimation: function() {
+		var tinfo = FCEM.timingInfo();
+
+		if (
+			// Emulated system is not 50 FPS (may be NTSC, or a bug)
+			Math.round(tinfo['system_fps'] / 1000) != 50 ||
+
+			// Difference between theroetical and observed framerate is too high
+			Math.abs(1 - (tinfo['ideal_frame_cnt'] / tinfo['frame_cnt'])) > .02
+		)
+		{
+			return {summary: 'very-bad', detail: tinfo};
+		}
+
+		// Emulator sometimes has to burst big batch of frames to catchup occasional sluggyness
+		if (tinfo['emulated_max'] > 6) {
+			return {summary: 'bad', detail: tinfo};
+		}
+
+		return {summary: 'good', detail: tinfo};
 	},
 };
 
@@ -595,6 +645,7 @@ function startAudioFromPhysical() {
   FCEM.createAudioContext();
   document.removeEventListener("keydown", startAudioFromPhysical);
   document.removeEventListener("click", startAudioFromPhysical);
+  hack_first_input_time = Date.now();
 }
 document.addEventListener("keydown", startAudioFromPhysical);
 document.addEventListener("click", startAudioFromPhysical);
